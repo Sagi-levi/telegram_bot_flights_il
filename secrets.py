03c823d037dd6dd3e6a62b10ec0f394536e777c2
Sagi-levi <71187499+Sagi-levi@users.noreply.github.com>
2022-04-30 19:55:36 +0300

    remove secrets

--- secrets.py
+++
@@ -1,6 +0,0 @@
-HEADERS = {
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    'x-rapidapi-key': "3c7a176513msh7d7f94d6f1be24dp143570jsn75bc41bccdc7"
-}
-API_KEY = '2090685122:AAGwD5pw2HE-kfVkxkT8JN3N3IcF-d2PBBU'
-CONNECTION_STRING = "mongodb+srv://SGLV:rxQC*3vPuXi38nc@yadstaim.gvmoh.mongodb.net/test"
